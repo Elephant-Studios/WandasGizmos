60e459687b81f42dc718e78e05adfec809a94947
Desymoo <Ionas177@gmail.com>
2024-06-22 00:38:00 -0700

    Initial refactor

--- CakeBuild/Program.cs
+++ CakeBuild/Program.cs
@@ -26,7 +26,7 @@ namespace CakeBuild
 
     public class BuildContext : FrostingContext
     {
-        public const string ProjectName = "VSModTemplate";
+        public const string ProjectName = "GrappleParkour";
         public string BuildConfiguration { get; }
         public string Version { get; }
         public string Name { get; }
