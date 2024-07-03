925ade2801ec9e9b16960495fbe7e9edd45883d5
Desymoo <Ionas177@gmail.com>
2024-07-02 20:30:12 -0700

    Project refactoring

--- CakeBuild/Program.cs
+++ CakeBuild/Program.cs
@@ -26,7 +26,7 @@ namespace CakeBuild
 
     public class BuildContext : FrostingContext
     {
-        public const string ProjectName = "GrappleParkour";
+        public const string ProjectName = "WandasGizmos";
         public string BuildConfiguration { get; }
         public string Version { get; }
         public string Name { get; }
